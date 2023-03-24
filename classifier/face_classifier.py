5a5ccfc420f95964a8b06be1b75ba25de09cfb95
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-03-24 11:32:34 -0500

    adding classifier and hog_imagen_recognition

---
+++ classifier/face_classifier.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+face_classifier = cv2.CascadeClassifier(
+    'src/opencv_xml_files/haarcascade_frontalface_alt.xml')
+resized = cv2.imread("src/images/varias_caras.jpg")
+gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+''' Nuestro clasificador devuelve el ROI de la cara detectada como una tupla, almacena 
+    la coordenada superior izquierda y la coordenada inferior derecha'''
+faces = face_classifier.detectMultiScale(gray, 1.0485258, 1)
+'''Cuando no se detectan caras, face_classifier regresa y la tupla vacía'''
+if faces is ():
+    print("No se han encontrado caras")
+'''Iteramos nuestra matriz de caras y dibujamos un rectángulo sobre cada cara en caras'''
+for (x, y, w, h) in faces:
+    cv2.rectangle(resized, (x, y), (x+w, y+h), (127, 0, 255), 2)
+    cv2.imshow('Face Detection', resized)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
